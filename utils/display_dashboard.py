2dd6c3c84401e916c9a5fc55f97ccd9eba01d3b9
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-02-24 15:02:36 +0100

    Update display_dashboard.py

--- utils/display_dashboard.py
+++ utils/display_dashboard.py
@@ -109,9 +109,9 @@ def map_combined_datasets(dataframes, filenames=None):
             mapbox=dict(
                 style="open-street-map",
                 center=dict(lat=center_lat, lon=center_lon),
-                zoom=6
+                zoom=3
             ),
-            legend=dict(title="Legenda", x=0, y=-0.1),
+            legend=dict(title="Legenda", x=1.05, y=1.5),
             height=800,
             margin={"r":0,"t":0,"l":0,"b":0}
         )
