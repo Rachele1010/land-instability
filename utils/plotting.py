ec45bb54e1933040a6775038ca01f88b61ccdf3d
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-02-25 18:05:34 +0100

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -108,9 +108,12 @@ def create_and_render_plot(df, x_axis, y_axis, plot_type):
         y_axis_bar = st.selectbox("Select Bar Y axis", df.columns.tolist(), key=f"y_axis_bar_{x_axis}")
         chart = create_mixed_line_and_bar_chart(df, x_axis, y_axis_line, y_axis_bar)
     elif plot_type == "Pie Chart":
-        chart = create_pie_chart(df, x_axis, y_axis)
+        chart = create_pie_chart(df, y_axis, x_axis)  # Inverti gli argomenti
+        st.plotly_chart(chart, use_container_width=True)
+        return
     elif plot_type == "Pie Chart (eCharts)":
-        chart =  render_pie_chart(df, y_axis, x_axis)
-        return chart
+        render_pie_chart(df, y_axis, x_axis)  # Non serve assegnare a "chart"
+        return  # Stop per evitare st.plotly_chart()
+
     st.plotly_chart(chart, use_container_width=True)
     st.stop
