34b2336af488c50b1efa7c7ff41c8f77ec8fe6f5
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-02-27 10:08:33 +0100

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -117,7 +117,9 @@ def create_and_render_plot(df, x_axis, y_axis, plot_type):
         y_axis_line = st.selectbox("Select Line Y axis", df.columns.tolist(), key=f"y_axis_line_{x_axis}")
         y_axis_bar = st.selectbox("Select Bar Y axis", df.columns.tolist(), key=f"y_axis_bar_{x_axis}")
         chart = create_mixed_line_and_bar_chart(df, x_axis, y_axis_line, y_axis_bar)
-    return chart
+
+    # Mostra il grafico prima di restituirlo
     st.plotly_chart(chart, use_container_width=True)
-    st.stop
+    return chart
+
 
