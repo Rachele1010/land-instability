3b9816125e2bb2fd61e2df7bcf80866661e6184c
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-02-27 17:46:14 +0100

    Update Statistics.py

--- utils/Statistics.py
+++ utils/Statistics.py
@@ -307,7 +307,7 @@ def Statistics(df_list, filenames):
                             aggregazioni = aggrega_dati_temporali(df, colonna_data, variabile_plot)
         
                         # Visualizzazione dei grafici per ogni aggregazione temporale
-                    with cole2:
+                    with col2:
                         for periodo, agg_df in aggregazioni.items():
                             st.write(f"#### Plot {periodo} by {dataset_name}")
                             fig = px.bar(agg_df, x=agg_df.index, y=agg_df.values, title=f"{periodo} Aggregate")
