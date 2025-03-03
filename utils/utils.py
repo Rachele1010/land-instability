f2b888c9ab8f33f258942e7a6b940e5d2a34a950
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-03-03 11:02:48 +0100

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -19,7 +19,7 @@ def convert_unix_to_datetime(df):
 # Funzione per calcolare l'autocorrelazione
 def compute_autocorrelation(df, column, max_lag=50):
     if column not in df.columns:
-        st.error(f"❌ Errore: La colonna '{column}' non esiste nel DataFrame.")
+        st.error(f"❌ Error: One of the selected columns does not exist in the DataFrame.")
         return None
     autocorr_values = [df[column].autocorr(lag) for lag in range(1, min(len(df), max_lag))]
     lags = list(range(1, len(autocorr_values) + 1))
@@ -27,14 +27,14 @@ def compute_autocorrelation(df, column, max_lag=50):
     
 def compute_cross_correlation(df, column1, column2, max_lag=50):
     if column1 not in df.columns or column2 not in df.columns:
-        st.error("❌ Errore: Una delle colonne selezionate non esiste nel DataFrame.")
+        st.error("❌ Error: One of the selected columns does not exist in the DataFrame.")
         return None
     cross_corr_values = [df[column1].corr(df[column2].shift(lag)) for lag in range(1, min(len(df), max_lag))]
     lags = list(range(1, len(cross_corr_values) + 1))
     return lags, cross_corr_values
 
 # Funzione per calcolare le statistiche
-def calcola_statistiche(df):
+def calcula_statistics(df):
     stats = []
     for col in df.columns:
         if pd.api.types.is_numeric_dtype(df[col]):
