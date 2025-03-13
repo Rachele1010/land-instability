5ee5e50453f50163f1d5445b7c3fe3c3ec247fbc
Rachele1010 <61459424+Rachele1010@users.noreply.github.com>
2025-03-13 10:31:52 +0100

    Update Statistics.py

--- script_app/statistics_map_combined/Statistics.py
+++ script_app/statistics_map_combined/Statistics.py
@@ -309,7 +309,7 @@ def Statistics_Data(df_list, filenames):
         if 'selected_distribution_datasets' not in st.session_state:
             st.session_state.selected_distribution_datasets = filenames  # Imposta il valore iniziale
     
-        selected_datasets = st.multiselect("Select datasets", filenames, default=st.session_state.selected_distribution_datasets)
+        selected_datasets = st.multiselect("Select datasets", filenames, default=filenames)
     
         # Selezioniamo solo i dataset che sono stati selezionati
         st.session_state.selected_distribution_datasets = selected_datasets
